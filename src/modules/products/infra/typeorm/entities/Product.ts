cc1a279681b6326248ba97ded1516f08707e06d9
Denis Souza <denyssouza1@hotmail.com>
2020-08-28 23:16:35 -0300

    entity products typeorm

--- src/modules/products/infra/typeorm/entities/Product.ts
+++ src/modules/products/infra/typeorm/entities/Product.ts
@@ -23,7 +23,7 @@ class Product {
   @Column('int')
   quantity: number;
 
-  // @OneToMany(() => OrdersProducts, ordersProducts => ordersProducts.product)
+  @OneToMany(() => OrdersProducts, ordersProducts => ordersProducts.product)
   order_products: OrdersProducts[];
 
   @CreateDateColumn()
